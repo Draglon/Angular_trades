d77bc898c5eaaa793b60c7c7f0f0d80dca6a8110
Sergey <s.nazarchuck@gmail.com>
2018-10-24 12:02:57 +0300

    add main sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+// Main component
 import { AppComponent }   from './app.component';
-import { HeaderComponent }   from './Header/header.component';
-import { FooterComponent }   from './Footer/footer.component';
+// Components
+import { HeaderComponent }   from './components/Header/header.component';
+import { FooterComponent }   from './components/Footer/footer.component';
+// Pages
+import { HomeComponent }   from './pages/Home/home.component';
+import { NotFoundComponent }   from './pages/NotFound/not-found.component';
+
+// определение маршрутов
+const appRoutes: Routes =[
+    { path: '', component: HomeComponent},
+    // { path: 'about', component: AboutComponent},
+    { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, HeaderComponent, FooterComponent ],
+    imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+    declarations: [ AppComponent, HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
