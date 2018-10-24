76e3d65686649e35a66b80201bd097fabe516f98
Sergey <s.nazarchuck@gmail.com>
2018-10-24 10:34:39 +0300

    add header component

---
+++ src/app/Header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'header-component',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.less']
+})
+
+export class HeaderComponent {
+    imgLogo: any = './src/assets/images/logo.jpg';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { HeaderComponent }   from './Header/header.component';
+import { FooterComponent }   from './Footer/footer.component';
+
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, HeaderComponent, FooterComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
